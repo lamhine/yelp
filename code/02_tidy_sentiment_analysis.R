2e06f61fbeca31107756696aa2fe9c7b29d57b2c
Tracy Lam-Hine <lamhine@stanford.edu>
2023-06-26 15:39:42 -0700

    Update 02_tidy_sentiment_analysis.R

--- code/02_tidy_sentiment_analysis.R
+++ code/02_tidy_sentiment_analysis.R
@@ -3,23 +3,47 @@ library(tidyverse)
 library(tidytext)
 
 # stream in 1 million randomly sampled reviews and all business data
-review_data_1M <- stream_in(file("/Users/lamhine/Downloads/archive/sample.json"))
+review_data <- stream_in(file("/Users/lamhine/Downloads/archive/yelp-sampled.json"))
 businesses <- stream_in(file("/Users/lamhine/Downloads/archive/yelp_academic_dataset_business.json"))
 
-# filter for only businesses containing "Chinese" in categories
-chinese <- businesses %>% filter(str_detect(categories, "Chinese"))
+# filter for only restuarants containing American, Chinese, Mexican, or Italian
+# in the `categories` variable
+
+restaurants <- businesses %>% 
+  filter(str_detect(categories, "Restaurants")) %>% 
+  filter(str_detect(categories, "American|Chinese|Mexican|Italian"))
 
 # inner_join with review data 
-chinese_revs <- inner_join(chinese, review_data_1M, by = "business_id")
+rest_revs <- inner_join(restaurants, review_data, by = "business_id")
 
 # keep only vars of interest
-chinese_revs <- chinese_revs %>% 
+rest_revs <- rest_revs %>% 
   select(business_id:review_count, 
          categories,
          review_id,
          stars.y,
          text,
          date)
+
+# get counts of words for each cuisine
+rest_revs <- rest_revs %>% 
+  mutate(cuis_count = str_count(categories, "American|Chinese|Mexican|Italian"))
+
+# filter out restaurants that overlap in multiple of these cuisine categories
+rest_uniq <- rest_revs %>% 
+  filter(cuis_count == 1)
+
+
+
+
+
+
+
+
+
+
+
+
 
 # unnest tokens and remove stop words
 data("stop_words")
