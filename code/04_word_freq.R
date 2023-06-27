2eab7f846579f1e9a71b77939111527c419438bd
Tracy Lam-Hine <lamhine@stanford.edu>
2023-06-27 13:42:18 -0700

    adding "dirty" to words

--- code/04_word_freq.R
+++ code/04_word_freq.R
@@ -7,6 +7,7 @@ freq_words <- rest_revs %>%
   filter(!is.na(text)) %>% 
   mutate(nasty = as.numeric(str_detect(text, "nasty")),
          gross = as.numeric(str_detect(text, "gross")),
+         dirty = as.numeric(str_detect(text, "dirty")),
          auth = as.numeric(str_detect(text, "authentic")))
 
 freq_sum <- freq_words %>% 
@@ -14,10 +15,11 @@ freq_sum <- freq_words %>%
   summarize(
     nasty = sum(nasty)*100 / n(),
     gross = sum(gross)*100 / n(),
+    dirty = sum(dirty)*100 / n(),
     auth = sum(auth)*100 / n()
   ) %>% 
   pivot_longer(
-    cols = c(2:4), 
+    cols = c(2:ncol(.)), 
     names_to = "word"
   )
     
@@ -25,5 +27,7 @@ plot_freq <- freq_sum %>%
   ggplot(aes(x = cuis_cat, y = value, fill = cuis_cat)) + 
   geom_bar(stat = "identity") + 
   facet_wrap(vars(word), scales="free")
+
+plot_freq
 
 ggsave("/Users/lamhine/Documents/GitHub/yelp/plots/plot_freq.png")
