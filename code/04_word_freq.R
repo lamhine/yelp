8a42b3b3929a302853fa65158f6e00df931dc53b
Tracy Lam-Hine <lamhine@stanford.edu>
2023-06-27 12:29:48 -0700

    updated code and data files

--- code/04_word_freq.R
+++ code/04_word_freq.R
@@ -3,9 +3,17 @@ setwd("/Users/lamhine/Documents/GitHub/yelp/data")
 rest_revs <- read_csv("rest_revs.csv")
 
 freq_words <- rest_revs %>% 
-  mutate(nasty = str_count(text, "nasty"),
-         gross = str_count(text, "gross"),
-         auth = str_count(text, "authentic"))
+  mutate(nasty = as.numeric(str_detect(text, "nasty")),
+         gross = as.numeric(str_detect(text, "gross")),
+         auth = as.numeric(str_detect(text, "authentic")))
+
+freq_words %>% 
+  group_by(cuis_cat) %>% 
+  summarize(
+    nasty_freq = sum(nasty) / n(),
+    gross_freq = sum(gross) / n(),
+    auth_freq = sum(auth) / n()
+  )
     
 plot_freq <- freq_words %>% 
   ggplot(aes(x = cuis_cat)) 
