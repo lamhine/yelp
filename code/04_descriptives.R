63f48fcd48bf5f6fb7935ed9a85c6723c25b8e6c
Tracy Lam-Hine <lamhine@stanford.edu>
2023-06-27 15:30:37 -0700

    update descriptives code

--- code/04_descriptives.R
+++ code/04_descriptives.R
@@ -1,15 +1,22 @@
+# load packages
 library(tidyverse)
-setwd("/Users/lamhine/Documents/GitHub/yelp/data")
-unzip("rest_revs.csv.zip")
-rest_revs <- read_csv("rest_revs.csv")
+library(data.table)
 
+# set working directory
+setwd("/Users/lamhine/Documents/GitHub/yelp/data")
+
+# load in data
+rest_revs <- fread("rest_revs.csv.gz")
+
+# create dummy variables for presence words of interest in each review
 freq_words <- rest_revs %>% 
-  filter(!is.na(text)) %>% 
   mutate(nasty = as.numeric(str_detect(text, "nasty")),
          gross = as.numeric(str_detect(text, "gross")),
          dirty = as.numeric(str_detect(text, "dirty")),
          auth = as.numeric(str_detect(text, "authentic")))
 
+# plot proportions of reviews by category that mention each word
+# first group by and summarize, then ggplot
 freq_sum <- freq_words %>% 
   group_by(cuis_cat) %>% 
   summarize(
@@ -53,3 +60,25 @@ plot_stars <- freq_stars %>%
 plot_stars
 ggsave("/Users/lamhine/Documents/GitHub/yelp/plots/plot_stars.png")
 
+# stratify plots by price category, remove NA or "None"
+freq_price <- freq_words %>% 
+  group_by(cuis_cat, attributes.RestaurantsPriceRange2) %>% 
+  summarize(
+    nasty = sum(nasty)*100 / n(),
+    gross = sum(gross)*100 / n(),
+    dirty = sum(dirty)*100 / n(),
+    auth = sum(auth)*100 / n()
+  ) %>% 
+  pivot_longer(
+    cols = c(3:ncol(.)), 
+    names_to = "word"
+  ) %>% 
+  filter(attributes.RestaurantsPriceRange2 %in% c(1:4))
+
+plot_price <- freq_price %>% 
+  ggplot(aes(x = cuis_cat, y = value, fill = factor(attributes.RestaurantsPriceRange2))) + 
+  geom_bar(stat = "identity", position = "dodge") + 
+  facet_wrap(vars(word), scales="free")
+
+plot_price
+ggsave("/Users/lamhine/Documents/GitHub/yelp/plots/plot_stars.png")
