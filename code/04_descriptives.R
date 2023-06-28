5f90bc9e4dbb11dada95c2242087a8e86d71cf79
Tracy Lam-Hine <lamhine@stanford.edu>
2023-06-28 15:56:58 -0700

    update code and plots

--- code/04_descriptives.R
+++ code/04_descriptives.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(data.table)
 library(gtsummary)
+library(webshot2)
 
 # set working directory
 setwd("/Users/lamhine/Documents/GitHub/yelp/data")
@@ -11,18 +12,27 @@ rest_revs <- fread("rest_revs.csv.gz")
 
 # create dummy variables for presence words of interest in each review
 freq_words <- rest_revs %>% 
-  mutate(nasty = as.numeric(str_detect(text, "nasty")),
-         gross = as.numeric(str_detect(text, "gross")),
+  mutate(text = tolower(text)) %>% 
+  mutate(ngdd = as.numeric(str_detect(text, "nasty|gross|disgust|disgusting")),
          dirty = as.numeric(str_detect(text, "dirty")),
+         badhor = as.numeric(str_detect(text, "bad|horrible")),
          auth = as.numeric(str_detect(text, "authentic")))
+
+# collapse the $$$ and $$$$ categories because too few in the $$$$ group
+freq_words <- freq_words %>% 
+  mutate(
+    attributes.RestaurantsPriceRange2 = case_when(
+      attributes.RestaurantsPriceRange2 == "4" ~ "3",
+      TRUE ~ attributes.RestaurantsPriceRange2)
+    )
 
 # create table 1
 tbl1 <- freq_words %>% 
   dplyr::rename(
     `Star rating` = stars.y,
     Price = attributes.RestaurantsPriceRange2,
-    Nasty = nasty,
-    Gross = gross,
+    `Nasty, gross, disgust*` = ngdd,
+    `Bad, horrible` = badhor,
     Dirty = dirty,
     Authentic = auth
          ) %>% 
@@ -36,12 +46,11 @@ tbl1 <- freq_words %>%
     Price = case_when(
       Price == 1 ~ "$",
       Price == 2 ~ "$$",
-      Price == 3 ~ "$$$",
-      Price == 4 ~ "$$$$",
+      Price == 3 ~ "$$$ and $$$$",
       TRUE ~ "Missing")
     ) %>% 
   select(cuis_cat, `Star rating`, Price, 
-         Nasty, Gross, Dirty, Authentic) %>% 
+         `Nasty, gross, disgust*`, `Bad, horrible`, Dirty, Authentic) %>% 
   tbl_summary(by = cuis_cat) %>% 
   add_overall()
 
@@ -54,10 +63,10 @@ tbl1 %>% as_gt() %>% gt::gtsave("/Users/lamhine/Documents/GitHub/yelp/plots/tbl1
 freq_sum <- freq_words %>% 
   group_by(cuis_cat) %>% 
   summarize(
-    nasty = sum(nasty)*100 / n(),
-    gross = sum(gross)*100 / n(),
-    dirty = sum(dirty)*100 / n(),
-    auth = sum(auth)*100 / n()
+    `Nasty, gross, disgust*` = sum(ngdd)*100 / n(),
+    `Bad, horrible` = sum(badhor)*100 / n(),
+    Dirty = sum(dirty)*100 / n(),
+    Authentic = sum(auth)*100 / n()
   ) %>% 
   pivot_longer(
     cols = c(2:ncol(.)), 
@@ -67,7 +76,11 @@ freq_sum <- freq_words %>%
 plot_freq <- freq_sum %>% 
   ggplot(aes(x = cuis_cat, y = value, fill = cuis_cat)) + 
   geom_bar(stat = "identity") + 
-  facet_wrap(vars(word), scales="free")
+  facet_wrap(vars(word), scales="free") +
+  theme(axis.title.x = element_blank()) + 
+  labs(title = "Reviews mentioning keywords by cuisine",
+       y = "Proportion",
+       fill = "Cuisine")
 
 plot_freq
 ggsave("/Users/lamhine/Documents/GitHub/yelp/plots/plot_freq.png")
@@ -76,10 +89,10 @@ ggsave("/Users/lamhine/Documents/GitHub/yelp/plots/plot_freq.png")
 freq_stars <- freq_words %>% 
   group_by(cuis_cat, stars.y) %>% 
   summarize(
-    nasty = sum(nasty)*100 / n(),
-    gross = sum(gross)*100 / n(),
-    dirty = sum(dirty)*100 / n(),
-    auth = sum(auth)*100 / n()
+    `Nasty, gross, disgust*` = sum(ngdd)*100 / n(),
+    `Bad, horrible` = sum(badhor)*100 / n(),
+    Dirty = sum(dirty)*100 / n(),
+    Authentic = sum(auth)*100 / n()
   ) %>% 
   pivot_longer(
     cols = c(3:ncol(.)), 
@@ -89,7 +102,11 @@ freq_stars <- freq_words %>%
 plot_stars <- freq_stars %>% 
   ggplot(aes(x = cuis_cat, y = value, fill = factor(stars.y))) + 
   geom_bar(stat = "identity", position = "dodge") + 
-  facet_wrap(vars(word), scales="free")
+  facet_wrap(vars(word), scales="free") +
+  theme(axis.title.x = element_blank()) + 
+  labs(title = "Reviews mentioning keywords by cuisine, stratified by review star rating",
+       y = "Proportion",
+       fill = "Review star rating")
 
 plot_stars
 ggsave("/Users/lamhine/Documents/GitHub/yelp/plots/plot_stars.png")
@@ -98,10 +115,10 @@ ggsave("/Users/lamhine/Documents/GitHub/yelp/plots/plot_stars.png")
 freq_price <- freq_words %>% 
   group_by(cuis_cat, attributes.RestaurantsPriceRange2) %>% 
   summarize(
-    nasty = sum(nasty)*100 / n(),
-    gross = sum(gross)*100 / n(),
-    dirty = sum(dirty)*100 / n(),
-    auth = sum(auth)*100 / n()
+    `Nasty, gross, disgust*` = sum(ngdd)*100 / n(),
+    `Bad, horrible` = sum(badhor)*100 / n(),
+    Dirty = sum(dirty)*100 / n(),
+    Authentic = sum(auth)*100 / n()
   ) %>% 
   pivot_longer(
     cols = c(3:ncol(.)), 
@@ -112,7 +129,11 @@ freq_price <- freq_words %>%
 plot_price <- freq_price %>% 
   ggplot(aes(x = cuis_cat, y = value, fill = factor(attributes.RestaurantsPriceRange2))) + 
   geom_bar(stat = "identity", position = "dodge") + 
-  facet_wrap(vars(word), scales="free")
+  facet_wrap(vars(word), scales="free") +
+  theme(axis.title.x = element_blank()) + 
+  labs(title = "Reviews mentioning keywords by cuisine, stratified by price range",
+       y = "Proportion",
+       fill = "Price range")
 
 plot_price
-ggsave("/Users/lamhine/Documents/GitHub/yelp/plots/plot_stars.png")
+ggsave("/Users/lamhine/Documents/GitHub/yelp/plots/plot_price.png")
